b1798ee613d89293ffc43b1421c4ce6f9036c9ee
Fernando Seguim <fernando@seguim.it>
2017-06-11 12:44:52 -0300

    Usando System.IO para ler arquivos

---
+++ EditorTexto/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EditorTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTextao = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTextao
+            // 
+            this.txtTextao.Location = new System.Drawing.Point(12, 12);
+            this.txtTextao.Multiline = true;
+            this.txtTextao.Name = "txtTextao";
+            this.txtTextao.Size = new System.Drawing.Size(366, 303);
+            this.txtTextao.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Salvar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 386);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTextao);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTextao;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EditorTexto/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditorTexto
+{
+    public partial class Form1 : Form
+    {
+        Stream textaoStream;
+        string file = "textao.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            if (File.Exists(file))
+            {
+                textaoStream = File.Open(file, FileMode.Open);
+                StreamReader streamReader = new StreamReader(textaoStream);
+                string line = streamReader.ReadLine();
+
+                while (line != null)
+                {
+                    txtTextao.Text += line;
+                    line = streamReader.ReadLine();
+                }
+
+                streamReader.Close();
+                textaoStream.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textaoStream = File.Open(file, FileMode.Create);
+            StreamWriter streamWriter = new StreamWriter(textaoStream);
+            streamWriter.Write(txtTextao.Text);
+
+            streamWriter.Close();
+            textaoStream.Close();
+        }
+
+
+    }
+}
